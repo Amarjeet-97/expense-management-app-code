cfeab5608da84a6884cdfcf41fbe5b76592361d8
Amarjeet-97 <106522356+Amarjeet-97@users.noreply.github.com>
2023-01-27 11:46:21 +0530

    create client

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-dark text-light p-4'>
+    <h6>All rights reserved &copy; amarjeet</h6>
+
+    </div>
+  )
+}
+
+export default Footer
